c353298dc806284482c200b1a888227a9af8275d
IrieY56 <IrieY56@gmail.com>
2018-12-01 20:05:03 -0500

    squares now fill out with X's on click

--- src/index.jsx
+++ src/index.jsx
@@ -5,16 +5,33 @@ import './index.css'
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button 
+      className="square" 
+      onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  state = {
+    squares: Array(9).fill(null),
+  }
+
+  handclick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares})
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handclick(i)}
+      />
+    );
   }
 
   render() {
